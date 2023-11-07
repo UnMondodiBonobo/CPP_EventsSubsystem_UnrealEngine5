6a9713671a6922d53fb75557e4db4294b899b87c
UnMondodiBonobo <alessandro.profera@gmail.com>
2023-11-07 22:18:15 +0100

    EventsSubsystem
    Added EventsSubsystem main functionalities

---
+++ EventsSubsystem/EventsSubsystem.cpp
@@ -0,0 +1,182 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OuterWilds/Subsystem/EventsSubsystem.h"
+#include <GameplayTagContainer.h>
+
+static TMap<FGameplayTag, TArray<FFunctionBinding>> BindingPairs;
+
+void UEventsSubsystem::Internal_BroadcastEventByTag(FGameplayTag InTag, void* InParams,	const FProperty* ParamType)
+{
+	if (!Internal_TagValidation(InTag))
+	{
+		return;
+	}
+
+	if (const auto* BoundObjects = BindingPairs.Find(InTag); BoundObjects && !BoundObjects->IsEmpty())
+	{
+		for (auto& BoundObject : *BoundObjects)
+		{
+			if (!BoundObject.IsValid())
+			{
+				continue;
+			}
+
+			if(!Internal_FunctionParamsMatchesEventParams(BoundObject.Func.Get(), ParamType))
+			{
+				continue;
+			}
+			
+			BoundObject.Obj->ProcessEvent(BoundObject.Func.Get(), InParams);
+		}
+	}
+}
+bool UEventsSubsystem::Internal_TagValidation(const FGameplayTag& InTag)
+{
+	if (!InTag.IsValid())
+	{
+#if WITH_EDITOR
+		UE_LOG(LogTemp, Warning, TEXT("Invalid Tag passed to EventsSubsystem"));
+#endif
+		return false;
+	}
+
+	return true;
+}
+
+bool UEventsSubsystem::Internal_FunctionParamsMatchesEventParams(const UFunction* InFunc,
+	const FProperty* InParamsProperty)
+{
+	int32 FuncInParamsNum = 0;
+	for (TFieldIterator<FProperty> It(InFunc); It; ++It)
+	{
+		const FProperty* FuncProperty = *It;
+		if (FuncProperty->PropertyFlags & CPF_Parm)
+		{
+			++FuncInParamsNum;
+			if(!FuncProperty->SameType(InParamsProperty))
+			{
+				return false;		
+			}
+		}
+	}
+
+	if(FuncInParamsNum == 0)
+	{
+		if(InParamsProperty)
+		{
+			return false;
+		}
+	}
+
+	return true;
+	
+}
+
+bool UEventsSubsystem::TryBindFunctionToChannel(FGameplayTag InTag, FName InNotifiedFunctionName, UObject* InNotifiedObject)
+{
+	check(InNotifiedObject);
+	if (!Internal_TagValidation(InTag))
+	{
+		return false;
+	}
+
+	if (InNotifiedFunctionName.IsNone())
+	{
+		return false;
+	}
+
+	UFunction* FoundFunction = InNotifiedObject->FindFunction(InNotifiedFunctionName);
+	if (!FoundFunction)
+	{
+		return false;
+	}
+
+	if (auto* BoundObjects = BindingPairs.Find(InTag))
+	{
+		if (!Algo::FindByPredicate(*BoundObjects, [&FoundFunction, &InNotifiedObject](const FFunctionBinding& Binding)
+		{
+			if (!Binding.IsValid())
+			{
+				return false;
+			}
+
+			return Binding == FFunctionBinding{InNotifiedObject, FoundFunction};
+		}))
+		{
+			BoundObjects->Emplace(InNotifiedObject, FoundFunction);
+			return true;
+		}
+		
+#if WITH_EDITOR
+			UE_LOG(LogTemp, Warning, TEXT("Trying to bind a function that is already bound to channel: %s"), *InTag.ToString());
+#endif
+		return false;
+		
+	}
+
+	return false;
+}
+
+bool UEventsSubsystem::TryCreateChannelByTag(FGameplayTag InTag)
+{
+	if (!Internal_TagValidation(InTag))
+	{
+		return false;
+	}
+
+	if (!BindingPairs.Find(InTag))
+	{
+		BindingPairs.Emplace(InTag, TArray<FFunctionBinding>{});
+		return true;
+	}
+
+#if WITH_EDITOR
+	UE_LOG(LogTemp, Warning, TEXT("Trying to create a channel that already exists"));
+#endif
+	return false;
+	
+}
+
+void UEventsSubsystem::BroadcastChannelEventByTag_Params(FGameplayTag InTag, const int32& Params)
+{
+	check(0);
+}
+
+void UEventsSubsystem::BroadcastChannelEventByTag_NoParams(FGameplayTag InTag)
+{
+	Internal_BroadcastEventByTag(InTag, nullptr, nullptr);
+}
+
+void UEventsSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+}
+
+void UEventsSubsystem::Deinitialize()
+{
+	BindingPairs.Empty();
+	Super::Deinitialize();
+}
+
+bool UEventsSubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+	return Super::ShouldCreateSubsystem(Outer);
+}
+
+DEFINE_FUNCTION(UEventsSubsystem::execBroadcastChannelEventByTag_Params)
+{
+	/*Create Tag that is going to be populated*/
+	FGameplayTag TagPtr = FGameplayTag{};
+	Stack.MostRecentProperty = nullptr;
+	/*Go one step forward in the stack, find the Tag and populate the pointer passed*/
+	Stack.Step(Stack.Object, &TagPtr);
+	/*Go another step forward in the stack and find the wildcard value*/
+	Stack.Step(Stack.Object, nullptr);
+	/*We don't care what the wildcard value is, just create a void pointer pointing to it*/
+	void* ParamAddress = Stack.MostRecentPropertyAddress;
+
+	P_FINISH
+	P_NATIVE_BEGIN;
+		Internal_BroadcastEventByTag(TagPtr, ParamAddress, Stack.MostRecentProperty);
+	P_NATIVE_END;
+}
---
+++ EventsSubsystem/EventsSubsystem.h
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <CoreMinimal.h>
+#include <Subsystems/GameInstanceSubsystem.h>
+#include <GameplayTagContainer.h>
+
+#include "EventsSubsystem.generated.h"
+
+struct FGameplayTag;
+
+USTRUCT()
+struct FFunctionBinding
+{
+	GENERATED_BODY()
+
+	FFunctionBinding() = default;
+	FFunctionBinding(UObject* InObj, UFunction* InFunc) : Obj(InObj), Func(InFunc) {}
+	
+	TWeakObjectPtr<UObject> Obj;
+	TWeakObjectPtr<UFunction> Func;
+
+	bool operator == (const FFunctionBinding& InBinding) const
+	{
+		return InBinding.Obj == Obj && InBinding.Func == Func;
+	}
+
+	bool IsValid() const
+	{
+		return Obj.IsValid() && Func.IsValid();
+	}
+
+	friend uint32 GetTypeHash(const FFunctionBinding& InFunctionBinding)
+	{
+		return HashCombine(GetTypeHash(InFunctionBinding.Obj), GetTypeHash(InFunctionBinding.Func));
+	}
+};
+
+UCLASS()
+class OUTERWILDS_API UEventsSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, meta = (DefaultToSelf = InNotifiedObject))
+	bool TryBindFunctionToChannel(FGameplayTag InTag, FName InNotifiedFunctionName, UObject* InNotifiedObject);
+	UFUNCTION(BlueprintCallable)
+	bool TryCreateChannelByTag(FGameplayTag InTag);
+	UFUNCTION(BlueprintCallable, CustomThunk,  meta = (CustomStructureParam = "Params"))
+	void BroadcastChannelEventByTag_Params(FGameplayTag InTag, const int32& Params);
+	UFUNCTION(BlueprintCallable)
+	void BroadcastChannelEventByTag_NoParams(FGameplayTag InTag);
+	
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+
+private:
+	static void Internal_BroadcastEventByTag(FGameplayTag InTag, void* InParams, const FProperty* ParamType);
+	static bool Internal_TagValidation(const FGameplayTag& InTag);
+	static bool Internal_FunctionParamsMatchesEventParams(const UFunction* InFunc, const FProperty* InParamsProperty);
+
+	DECLARE_FUNCTION(execBroadcastChannelEventByTag_Params);
+	
+};
